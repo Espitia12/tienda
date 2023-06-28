79396718471200da635f2c64011e283c4c6ab415
johan <johanespita@gmail.com>
2023-06-27 23:45:46 -0500

    ayadiendo el modificaciones2

--- js/compraB.js
+++ js/compraB.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Obtén todos los botones de compra
+
   var buyButtons = document.querySelectorAll(".card .buy-button");
   var cartCount = document.getElementById("cart-count");
 
-  // Agrega un evento de clic a cada botón de compra
+
   buyButtons.forEach(function(button) {
     button.addEventListener("click", function() {
-      // Obtén el número actual del carrito
+
       var currentCount = parseInt(cartCount.textContent);
 
-      // Incrementa el número del carrito
+
       var newCount = currentCount + 1;
 
-      // Actualiza el número del carrito en la interfaz
+
       cartCount.textContent = newCount;
 
-      // Muestra un mensaje de compra realizada
+
       alert("¡Has comprado el producto!");
     });
   });
